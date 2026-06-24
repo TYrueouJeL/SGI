992929f15790e112796723dc82ef1e8931bf6614
TYrueouJeL <guerinremi.pro@gmail.com>
2026-06-24 13:25:20 +0200

    PostgresSQL config

--- app/apps/backend/config/database.ts
+++ app/apps/backend/config/database.ts
@@ -5,7 +5,7 @@ const dbConfig = defineConfig({
   /**
    * Default connection used for all queries.
    */
-  connection: 'sqlite',
+  connection: 'pg',
 
   connections: {
     /**
@@ -52,21 +52,21 @@ const dbConfig = defineConfig({
      * PostgreSQL connection.
      * Install package to switch: npm install pg
      */
-    // pg: {
-    //   client: 'pg',
-    //   connection: {
-    //     host: process.env.PG_HOST,
-    //     port: Number(process.env.PG_PORT || 5432),
-    //     user: process.env.PG_USER,
-    //     password: process.env.PG_PASSWORD,
-    //     database: process.env.PG_DB_NAME,
-    //   },
-    //   migrations: {
-    //     naturalSort: true,
-    //     paths: ['database/migrations'],
-    //   },
-    //   debug: app.inDev,
-    // },
+    pg: {
+      client: 'pg',
+      connection: {
+        host: process.env.PG_HOST,
+        port: Number(process.env.PG_PORT || 5432),
+        user: process.env.PG_USER,
+        password: process.env.PG_PASSWORD,
+        database: process.env.PG_DB_NAME,
+      },
+      migrations: {
+        naturalSort: true,
+        paths: ['database/migrations'],
+      },
+      debug: app.inDev,
+    },
 
     /**
      * MySQL / MariaDB connection.
